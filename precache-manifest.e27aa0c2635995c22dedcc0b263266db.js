8846e47cfd340af962c0c2ba35453de80ef5522b
Ganesan Subbiahpandian <ganesanece@gmail.com>
2020-07-07 02:14:13 -0400

    first commit

---
+++ precache-manifest.e27aa0c2635995c22dedcc0b263266db.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1e9145e8ab1f760055c10963407924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c0e3aa3e7499a378575",
+    "url": "/static/css/2.b27f16ba.chunk.css"
+  },
+  {
+    "revision": "92af62d1d145bd059a69",
+    "url": "/static/css/main.51ec722d.chunk.css"
+  },
+  {
+    "revision": "2c0e3aa3e7499a378575",
+    "url": "/static/js/2.7ff9075e.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/static/js/2.7ff9075e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92af62d1d145bd059a69",
+    "url": "/static/js/main.7a70e9dd.chunk.js"
+  },
+  {
+    "revision": "477d012c49bb29965030",
+    "url": "/static/js/runtime-main.9a5eacd4.js"
+  },
+  {
+    "revision": "816c4c0f04f0878c4e2ff0c244ba16b0",
+    "url": "/static/media/HelveticaNeue.816c4c0f.ttc"
+  },
+  {
+    "revision": "ad7a8a395c3d941120f13c9cd88b9c8d",
+    "url": "/static/media/HelveticaNeueDeskInterface.ad7a8a39.ttc"
+  },
+  {
+    "revision": "8741c02b5edc0f21fa59eeff49e0d0a1",
+    "url": "/static/media/acherusgrotesque-regita.8741c02b.otf"
+  },
+  {
+    "revision": "fcdbb6ec21b6e14d37235ece1ddc1d83",
+    "url": "/static/media/acherusgrotesque-regular.fcdbb6ec.otf"
+  },
+  {
+    "revision": "8512c33fdfe5dcfd1d087d6daf5945cd",
+    "url": "/static/media/background.8512c33f.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "01bee9655bf434da91a4f4f5eed32339",
+    "url": "/static/media/gilroy-extrabold.01bee965.otf"
+  },
+  {
+    "revision": "d90fa984926d3745c7e68bc525692c26",
+    "url": "/static/media/gilroy-light.d90fa984.otf"
+  },
+  {
+    "revision": "4f104e92c32e9afe8c997910a7e2e8ae",
+    "url": "/static/media/image1.4f104e92.png"
+  },
+  {
+    "revision": "27a3fc2f2a14e9aed4cc445f435f5619",
+    "url": "/static/media/image5.27a3fc2f.png"
+  }
+]);
